20014ae597ae762ade99f5b8c46da0ade231f828
SAMIE MEHEZA <meheza.samie@utt.fr>
2015-05-14 22:56:36 +0200

    Intégration des ingredients
    - Ajout de Ingredient (=> list.html.twig, => show.html.twig)
    - Ajout de IngredientController.php
    - Modification de routine ( Ajout de route => app_dev/ingredient/list
    et => app_dev/ingredient/show/{libelle})
    - Ajout du lien ingredient dans header

---
+++ src/MealSquare/RecetteBundle/Controller/IngredientController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MealSquare\RecetteBundle\Controller;
+
+use MealSquare\RecetteBundle\Entity\Ingredient;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IngredientController extends Controller {
+
+    public function listAction(Request $request) {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $dql = "SELECT a FROM MealSquareRecetteBundle:Ingredient a";
+        $query = $em->createQuery($dql);
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+                $query, $request->query->get('page', 1)/* page number */, 12/* limit per page */
+        );
+
+        // parameters to template
+        return $this->render('MealSquareRecetteBundle:Ingredient:list.html.twig', array('pagination' => $pagination));
+    }
+
+    public function showAction($libelle) {
+        $repository = $this->getDoctrine()
+                ->getRepository("MealSquareRecetteBundle:Ingredient");
+        $ingredient = $repository->findOneBy(array('libelle' => $libelle));
+        
+        return $this->render('MealSquareRecetteBundle:Ingredient:show.html.twig', array('ingredient' => $ingredient));
+    }
+
+}
