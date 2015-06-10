c0671518d5498c0efaa44e0cd8d520e31e47d6a3
SAMIEMEHEZA <meheza.samie@utt.fr>
2015-06-10 14:32:59 +0200

    Intégration page d'accueil v1

--- src/MealSquare/CommonBundle/Controller/DefaultController.php
+++ src/MealSquare/CommonBundle/Controller/DefaultController.php
@@ -3,16 +3,31 @@
 namespace MealSquare\CommonBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use MealSquare\RecetteBundle\Entity\Recette;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('MealSquareCommonBundle:Default:index.html.twig');
+class DefaultController extends Controller {
+
+    public function indexAction() {
+        
+        $em = $this->getDoctrine()->getManager();
+        $recetteRepo = $em->getRepository('MealSquareRecetteBundle:Recette');
+        $ingredientRepo = $em->getRepository('MealSquareRecetteBundle:Ingredient');
+        $userRepo = $em->getRepository('ApplicationSonataUserBundle:User');
+        $nbuser = $userRepo->createQueryBuilder('l')
+                        ->select('COUNT(l)')
+                        ->getQuery()
+                        ->getSingleScalarResult();
+        $nbrecette = $recetteRepo->getNb();
+        $nbingredient = $ingredientRepo->getNb();
+        $idrecetteofday =  $recetteRepo->getDayRecipe();
+        $recette_de_la_journee = $recetteRepo->findOneById($idrecetteofday);
+        
+        return $this->render('MealSquareCommonBundle:Default:index.html.twig',array('nbrecette' => $nbrecette,
+            'nbingredient' => $nbingredient,'nbuser' => $nbuser,'recette_de_la_journee' => $recette_de_la_journee));
     }
-    
-    public function contactAction()
-    {
+
+    public function contactAction() {
         return $this->render('MealSquareCommonBundle:Default:contact.html.twig');
     }
+
 }
